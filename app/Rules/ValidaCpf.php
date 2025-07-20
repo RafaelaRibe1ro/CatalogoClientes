98de573a9c58a139ec9db9cb00b47db6f24a7321
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2025-07-20 01:05:12 -0300

    feat: criando função de validar cpf

---
+++ app/Rules/ValidaCpf.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidaCpf implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        // Remove caracteres não numéricos
+        $cpf = preg_replace('/[^0-9]/', '', $value);
+
+        if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function message()
+    {
+        return 'O CPF informado é inválido.';
+    }
+}
+
