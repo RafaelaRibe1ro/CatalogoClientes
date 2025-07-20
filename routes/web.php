d363c631819a4e628dba928dd76fdc106865dee9
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2025-07-20 01:02:12 -0300

    feat: adicionando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect('/clientes') : redirect('/login');
+});
+
+// Rotas de login/logout
+Auth::routes(['register' => false]);
+
+// Rotas protegidas
+Route::middleware('auth')->group(function () {
+    Route::resource('clientes', ClienteController::class);
 });
