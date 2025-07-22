f7e4e0a1aa8a8dbc7385733517064953098ea282
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2025-07-21 21:38:07 -0300

    feat: adicionando rota de subcategoria

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Subcategoria;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/subcategorias/{categoria}', function ($categoria) {
+    return Subcategoria::where('categoria_id', $categoria)
+           ->orderBy('nome')->get(['id','nome']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
